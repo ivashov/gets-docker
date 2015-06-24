a8823e300c3b766e37c0e3a39139fe99c389f6d0
Kirill Ivashov <ivashov@cs.karelia.ru>
2015-06-24 08:28:07 +0300

    Fix sib container kill

--- sib/run.sh
+++ sib/run.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
+trap 'kill $SLEEP_PID' INT TERM
+
 export LD_LIBRARY_PATH=/usr/local/lib
 eval `dbus-launch --sh-syntax`
+
+PID_DBUS=$DBUS_SESSION_BUS_PID
+
 redsibd&
-sleep 0.1
-sib-tcp --port 10622
+PID_REDSIB=$!
+
+sleep 1
+sib-tcp --port 10622&
+PID_SIB_TCP=$!
+
+while :
+do
+    sleep 60
+done&
+
+SLEEP_PID=$!
+
+wait $SLEEP_PID
+wait $SLEEP_PID
+
+echo "Killing SIB TCP"
+kill $PID_SIB_TCP
+wait $PID_SIB_TCP
+
+echo "Killing REDSIB"
+kill $PID_REDSIB
+wait $PID_REDSIB
+
+echo "Killing DBUS"
+kill $PID_DBUS
+wait $PID_DBUS
